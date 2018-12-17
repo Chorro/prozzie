6bcc5f6111cd641e98271ccf3380ef7ea258cf17
Eugenio Pérez <eupm90@gmail.com>
2018-12-17 20:29:13 +0100

    Print error stacktrace on random shunit errors
    Errors caused by `set -e` flag caused shunit NOT to print error line,
    making them difficult to debug. This commit solves that.

--- tests/test_run.sh
+++ tests/test_run.sh
@@ -9,10 +9,34 @@ oneTimeSetUp () {
 	bashopts=$(set +o)
 	declare -r bashopts
 	set -euf -o pipefail
+	set -o errtrace  # Needed for inherit ERR trap
 }
 
 oneTimeTearDown () {
 	$bashopts
+}
+
+# Print stack trace in case of sudden error
+trap 'print_stacktrace ${LINENO};' ERR
+
+print_stacktrace () {
+	printf 'SHUNIT ERROR STACKTRACE ($$: %s, BASHPID: %s): ================\n' \
+															 "$$" "$BASHPID" >&2
+	declare -i i
+	for (( i=1; i<${#FUNCNAME[@]}; ++i )); do
+		if [[ ${FUNCNAME[$i]} == '_shunit_execSuite' ]]; then
+			break
+		fi
+
+		if [[ $i -eq 1 ]]; then
+			declare lineno=$1
+		else
+			declare line_i=$((i-1))
+			declare lineno=${BASH_LINENO[$line_i]}
+		fi
+		printf '%s:%s\n' "${FUNCNAME[$i]}" "${lineno}" >&2
+	done
+	printf 'END OF SHUNIT STACKTRACE: =========\n' >&2
 }
 
 # Circleci + centos7 terminal does not do well, need to set TERM variable
